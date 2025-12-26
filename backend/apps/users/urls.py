0919f11adef9f96fb062bb266b9f31d0f8e35b70
“jaironberiguete” <Jaironberiguete@gmail.com>
2025-12-26 00:24:07 -0500

    now user register endpoind it's created end user can register and logs in

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import CurrentUserView
+from .views import CurrentUserView, RegisterView
 # If you made a custom serializer view, import that instead
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("user/", CurrentUserView.as_view(), name="current-user"),
+    path("register/", RegisterView.as_view(), name="register"),
+
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated
+from .serializers import RegisterSerializer
 
 class CurrentUserView(APIView):
     permission_classes = [IsAuthenticated]
@@ -13,3 +15,7 @@ class CurrentUserView(APIView):
             "first_name": user.first_name,
             "last_name": user.last_name
         })
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
