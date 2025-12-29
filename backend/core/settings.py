d582547b0623ab4e6511d4400596e56ac454c59f
“jaironberiguete” <Jaironberiguete@gmail.com>
2025-12-28 21:28:42 -0500

    setup postgress as the database now

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mystyle_db',
+        'USER': 'mystyle_user',
+        'PASSWORD': 'strongpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
